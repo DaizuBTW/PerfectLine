b2aac5ae469a0c0abe5eb36e41d27ecaa95d766c
Danila Selvanovich <danila.selvanovich@gmail.com>
2024-04-19 11:21:15 +0300

    Modify mail pattern

--- controller/form-processing-server.php
+++ controller/form-processing-server.php
@@ -7,7 +7,8 @@ $message = $_POST['message'];
 
 $to = 'PerfectLine@gmail.com';
 $subject = 'Contact form';
+$subject = "=?utf-8?B?" . base64_encode($subject) . "?=";
 $message = $message . " From: $name ($tel)";
-$headers = "From: $email" . "\r\n" . "Reply-To: $email" . "\r\n";
+$headers = "From: $email" . "\r\n" . "Reply-To: $email" . "\r\n" . "Content-type: text/plain; charset=utf-8" . "\r\n";
 
 mail($to, $subject, $message, $headers);
